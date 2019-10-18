1dffd3f64235e514129270e0e9f3debb640e9875
void <silentnewborn@163.com>
2019-10-18 16:38:50 +0800

    update scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public Transform m_pacman;
+    public Transform m_startPos;
+    public Transform m_endPos;
+    public List<Transform> m_eated;
+    public float m_speed = 0.1f;
+    public bool m_loadEnd = false;
+
+    void Start()
+    {
+        StartCoroutine(loading());
+    }
+
+    void Update()
+    {
+        Vector2 p = Vector2.MoveTowards(m_pacman.position, m_endPos.position, m_speed);
+        m_pacman.position = p;
+        if (Vector2.Distance(m_pacman.position, m_endPos.position) < 0.1f)
+        {
+            m_pacman.position = m_startPos.position;
+            foreach (var item in m_eated)
+            {
+                item.gameObject.SetActive(true);
+            }
+
+            m_loadEnd = true;
+        }
+    }
+
+    private IEnumerator loading()
+    {
+        string sceneName = PlayerPrefs.GetString("Scene");
+        print(sceneName);
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+        op.allowSceneActivation = false;
+        while (op.progress < 0.9f || !m_loadEnd)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        op.allowSceneActivation = true;
+    }
+}
