119f562f2df9566ac29073ffa61aa79f670905c8
void <silentnewborn@163.com>
2019-12-20 19:55:34 +0800

    添加敌人ai

--- Assets/Scripts/Ghost/Ghost.cs
+++ Assets/Scripts/Ghost/Ghost.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
@@ -11,18 +10,15 @@ public class Ghost : MonoBehaviour
         var pacmanState = other.transform.GetComponent<PacmanMove>().m_pacmanState;
         if (pacmanState == PacmanMove.PacmanNormal)
         {
+            other.GetComponent<PacmanMove>().ChangeState(PacmanMove.PacmanHurt);
             PacmanMove.m_life--;
-            if (PacmanMove.m_life <= 0) 
-            {
-                GameManager.gameManager.SaveHighScore();
-                WinCondiction.Instant.Death();
-                restartGameObject.SetActive(true);
-                Destroy(other.gameObject);
-            }
-            else
-            {
-                other.GetComponent<PacmanMove>().ChangeState(PacmanMove.PacmanHurt);
-            }
+            if (PacmanMove.m_life > 0) return;
+            
+            //生命为空是结束游戏
+            GameManager.gameManager.SaveHighScore();
+            WinCondiction.Instant.Death();
+            restartGameObject.SetActive(true);
+            Destroy(other.gameObject);
         }
         else if (pacmanState == PacmanMove.PacmanInvincible)
         {
@@ -30,5 +26,33 @@ public class Ghost : MonoBehaviour
             GameManager.gameManager.ghostRevenge(gameObject);
             gameObject.SetActive(false);
         }
+    }
+    
+    public Transform EscapeToNearWayPoint()
+    {
+        var wayPoints =  GameObject.FindGameObjectsWithTag("WayPoint");
+        var nearWay = transform;
+        var distance = float.MaxValue;
+        foreach (var item in wayPoints)
+        {
+            if (!(Vector2.Distance(item.transform.position, transform.position) < distance)) continue;
+            distance = Vector2.Distance(item.transform.position, transform.position);
+            nearWay = item.transform;
+        }
+        return nearWay;
+    }
+
+    public Transform ChangeFarWayPoint(Transform pacman,Transform nowPosition)
+    {
+        var nextWay = transform;//下一个路点，设为上一路点仅是为了避免报空值错误
+        float distance = 0;//初始距离设为最大值
+        var nextWays = nowPosition.GetComponent<WayPoint>();
+        foreach (var wayPoint in nextWays.nextPoint)//遍历该路点临近所有路点，找出与吃豆人最近的路点，设置为新路点
+        {
+            if (!(Vector2.Distance(wayPoint.position, pacman.position) > distance)) continue;
+            distance = Vector2.Distance(wayPoint.position, pacman.position);
+            nextWay = wayPoint;
+        }
+        return nextWay;
     }
 }
---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 地图航点，用于寻路
+/// </summary>
+public class WayPoint : MonoBehaviour
+{
+    public List<Transform> nextPoint;
+}
